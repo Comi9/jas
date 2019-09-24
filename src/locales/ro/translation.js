0badbadf37dce35d93443b44b0dc7e917c0fbf1e
Cosmin Popescu <cosminp9@gmail.com>
2019-09-24 21:55:45 +0300

    Initial commit from Create React App

---
+++ src/locales/ro/translation.js
@@ -0,0 +1,12 @@
+export const translationRO = {
+  'Performance Arts Journal': 'Jurnalul Artelor Spectacolului',
+  'Department of Drama and Theatre Studies': 'Departamentul de Arta Teatrala',
+  'Index': 'Acasa',
+  'About': 'Despre',
+  'Boards': 'Borduri',
+  'Instructions': 'Instructiuni',
+  'Review': 'Analiza',
+  'Call For Papers': 'Call For Papers',
+  'Archive': 'Arhiva',
+  'Contact': 'Contact'
+}
