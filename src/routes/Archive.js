7f3f46b014c948448c95daeda96d3a75d7de4808
Cosmin Popescu <cosminp9@gmail.com>
2019-10-14 13:13:44 +0300

    Archive page content update

--- src/routes/Archive.js
+++ src/routes/Archive.js
@@ -9,32 +9,27 @@ class Archive extends Component {
     return (
       <>
         <h1>{t('menu.archive')}</h1>
+
         <h3 className='secondary-color'>{t('2019')}</h3>
         <section className='archive-list'>
-          <div className='archive-item'>
+          <a className='archive-item' href='public/jas2019-1.pdf' target='_blank'>
             <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
+            <div className='description'>Nr. 1, 2019</div>
+          </a>
+          <a className='archive-item' href='public/jas2019-1.pdf' target='_blank'>
             <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
-            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
-            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
-            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
-            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
-          <div className='archive-item'>
-            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
-          </div>
+            <div className='description'>Nr. 2, 2019</div>
+          </a>
         </section>
 
         <h3 className='secondary-color'>{t('201x')}</h3>
+        <section className='archive-list'>
+          <div className='archive-item'>
+            <img src='/images/JAS-1-2019-wb.png' title='JAS' alt='JAS' />
+            <div className='description'>Nr. 1, 2015</div>
+          </div>
+        </section>
+
         <h3 className='secondary-color'>{t('201x')}</h3>
         <h3 className='secondary-color'>{t('201x')}</h3>
       </>
